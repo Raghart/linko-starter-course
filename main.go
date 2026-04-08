935134a497aa8f5eb82f7778c4a988da9cd04929
Raghart <rafaelrojas36@outlook.com>
2026-04-07 21:27:27 -0400

    r changed old log with slog logging system

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"fmt"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
@@ -25,25 +26,14 @@ func main() {
 }
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	envVal := os.Getenv("LINKO_LOG_FILE")
-	multiLogger, closeFunc, err := initializeLogger(envVal)
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+	st, err := store.New(dataDir, logger)
 	if err != nil {
-		log.Print(err)
+		logger.Error(fmt.Sprintf("failed to create store: %v\n", err))
 		return 1
 	}
-
-	defer func() {
-		if err := closeFunc(); err != nil {
-			log.Printf("There was a problem while trying to flush the helper: %v", err)
-		}
-	}()
-
-	st, err := store.New(dataDir, multiLogger)
-	if err != nil {
-		multiLogger.Printf("failed to create store: %v\n", err)
-		return 1
-	}
-	s := newServer(*st, httpPort, cancel, multiLogger)
+	s := newServer(*st, httpPort, cancel, logger)
 	var serverErr error
 	go func() {
 		serverErr = s.start()
@@ -51,14 +41,14 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	<-ctx.Done()
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	multiLogger.Println("Linko is shutting down")
+	logger.Info("Linko is shutting down")
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		multiLogger.Printf("failed to shutdown server: %v\n", err)
+		logger.Info(fmt.Sprintf("failed to shutdown server: %v\n", err))
 		return 1
 	}
 	if serverErr != nil {
-		multiLogger.Printf("server error: %v\n", serverErr)
+		logger.Info(fmt.Sprintf("server error: %v\n", serverErr))
 		return 1
 	}
 	return 0
