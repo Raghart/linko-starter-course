7abe88ea85b9052e67b6be855877bde19f1b57a5
Raghart <rafaelrojas36@outlook.com>
2026-04-13 16:47:56 -0400

    r added env and hostname to logging for more context

--- main.go
+++ main.go
@@ -50,9 +50,14 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 		infoHandler,
 	))
 
+	env := os.Getenv("ENV")
+	hostname, _ := os.Hostname()
+
 	logger = logger.With(
 		slog.String("git_sha", build.GitSHA),
 		slog.String("build_time", build.BuildTime),
+		slog.String("env", env),
+		slog.String("hostname", hostname),
 	)
 
 	st, err := store.New(dataDir, logger)
