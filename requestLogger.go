975d5057f7ab52a7484df8642ad62cc4d65eabd8
Raghart <rafaelrojas36@outlook.com>
2026-04-13 18:38:39 -0400

    r added spies to req and res to add more context to the requests

--- requestLogger.go
+++ requestLogger.go
@@ -3,16 +3,28 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"time"
 )
 
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
+			start := time.Now()
+
+			spyReader := &spyReadClose{ReadCloser: r.Body}
+			r.Body = spyReader
+
+			spyWriter := &spyResponseWriter{ResponseWriter: w}
+			next.ServeHTTP(spyWriter, r)
+
 			logger.Info("Served request",
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("client_ip", r.RemoteAddr),
+				slog.Duration("duration", time.Since(start)),
+				slog.Int("request_body_bytes", spyReader.bytesRead),
+				slog.Int("response_status", spyWriter.statusCode),
+				slog.Int("response_body_bytes", spyWriter.bytesWritten),
 			)
 		})
 	}
